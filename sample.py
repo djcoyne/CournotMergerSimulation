ee265b904aeaa53d086d3351106ab90b3c55aeab
David Coyne <djcoyne@gmail.com>
2017-12-12 14:08:06 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,4 +1,4 @@
-import CournotMerge
+import CournotMerge as cm
 
 # Define the markets (use the same order for elasticities, prices, and firm quantities below)
 markets = ["San Diego", "Seattle", "DC"]
@@ -11,10 +11,10 @@ price = [0.66, 1.20, 1.90]
 
 # Define the Firms as a dictionary starting with index 1 (Name, quantities, markets)
 firms={}
-firms[1] = cfirm("Trader Joe's", [45, 25, 15], markets)
-firms[2] = cfirm("Albertson's", [40, 18, 6], markets)
-firms[3] = cfirm("Whole Foods", [55, 45, 19], markets)
-firms[4] = cfirm("Sprouts", [11, 7, 4], markets)
+firms[1] = cm.firm("Trader Joe's", [45, 25, 15], markets)
+firms[2] = cm.firm("Albertson's", [40, 18, 6], markets)
+firms[3] = cm.firm("Whole Foods", [55, 45, 19], markets)
+firms[4] = cm.firm("Sprouts", [11, 7, 4], markets)
 
 # Identify the indices for the two parties that are merging
 merge_index = [1,3]
@@ -23,7 +23,7 @@ merge_index = [1,3]
 efficiency = 10
 
 # Call the merger program
-CM = cournotMerge(F=firms, markets=markets, e=elasticity, p=price, MF=merge_index, E=efficiency)
+CM = cm.cournotMerge(F=firms, markets=markets, e=elasticity, p=price, MF=merge_index, E=efficiency)
 
 # Describe the merger simulation
 print(CM.describe())
