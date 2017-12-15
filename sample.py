a4d0fa3fdb89c24b099c6ee0b86a65863241851b
David Coyne <djcoyne@gmail.com>
2017-12-15 11:25:49 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -20,6 +20,7 @@ firms[1] = cm.firm("Merchant Rick's", [45, 25, 15], markets)
 firms[2] = cm.firm("Dubertson's", [40, 18, 6], markets)
 firms[3] = cm.firm("Half Foods", [55, 45, 19], markets)
 firms[4] = cm.firm("Seedlings", [11, 7, 4], markets)
+firms[5] = cm.firm("Krugels", [38, 23, 10], markets)
 
 # Identify the indices for the two parties that are merging
 merge_index = [1,3]
