2c4a980e9e2c4a1edc205a903f2af3a66d8e225d
David Coyne <djcoyne@gmail.com>
2017-12-15 15:52:51 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -16,11 +16,11 @@ price = [0.66, 1.20, 1.90]
 
 # Define the Firms as a dictionary (Name, quantities, markets)
 firms={}
-firms['MR'] = cm.firm("Merchant Rick's", [45, 25, 15], markets)
-firms['D'] = cm.firm("Dubertson's", [40, 18, 6], markets)
-firms['HF'] = cm.firm("Half Foods", [55, 45, 19], markets)
-firms['S'] = cm.firm("Seedlings", [11, 7, 4], markets)
-firms['K'] = cm.firm("Krugel", [38, 23, 10], markets)
+firms[1] = cm.firm("Merchant Rick's", [45, 25, 15], markets)
+firms[2] = cm.firm("Dubertson's", [40, 18, 6], markets)
+firms[3] = cm.firm("Half Foods", [55, 45, 19], markets)
+firms[4] = cm.firm("Seedlings", [11, 7, 4], markets)
+firms[5] = cm.firm("Krugel", [38, 23, 10], markets)
 
 # Identify the keys for the two parties that are merging
 merge_keys = ['MR','HF']
